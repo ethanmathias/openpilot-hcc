18b4604b82468c9eae3e4b2feedb5ab8cbbb544c
Ethan Mathias <ethanmathias@gmail.com>
2026-02-10 16:47:50 -0500

    update

--- tools/sim/bridge/metadrive/metadrive_bridge.py
+++ tools/sim/bridge/metadrive/metadrive_bridge.py
@@ -81,7 +81,7 @@ class MetaDriveBridge(SimulatorBridge):
       crash_vehicle_done=False,
       crash_object_done=False,
       arrive_dest_done=False,
-      traffic_density=0.0, # traffic is incredibly expensive
+      traffic_density=1, # traffic is incredibly expensive
       map_config=create_map(),
       decision_repeat=1,
       physics_world_step_size=self.TICKS_PER_FRAME/100,
