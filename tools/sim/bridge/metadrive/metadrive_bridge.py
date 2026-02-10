c43dfe8cac59569c6652088aae56a993f7a2f958
Ethan Mathias <ethanmathias@gmail.com>
2026-02-10 16:54:08 -0500

    update

--- tools/sim/bridge/metadrive/metadrive_bridge.py
+++ tools/sim/bridge/metadrive/metadrive_bridge.py
@@ -31,18 +31,18 @@ def create_map(track_size=60):
   curve_len = track_size * 2
   return dict(
     type=MapGenerateMethod.PG_MAP_FILE,
-    lane_num=2,
+    lane_num=1,
     lane_width=4.5,
     config=[
       None,
       straight_block(track_size),
-      curve_block(curve_len, 90),
-      straight_block(track_size),
-      curve_block(curve_len, 90),
-      straight_block(track_size),
-      curve_block(curve_len, 90),
-      straight_block(track_size),
-      curve_block(curve_len, 90),
+      # curve_block(curve_len, 90),
+      # straight_block(track_size),
+      # curve_block(curve_len, 90),
+      # straight_block(track_size),
+      # curve_block(curve_len, 90),
+      # straight_block(track_size),
+      # curve_block(curve_len, 90),
     ]
   )
 
@@ -81,7 +81,7 @@ class MetaDriveBridge(SimulatorBridge):
       crash_vehicle_done=False,
       crash_object_done=False,
       arrive_dest_done=False,
-      traffic_density=0.0, # traffic is incredibly expensive
+      traffic_density=1.0, # traffic is incredibly expensive
       map_config=create_map(),
       decision_repeat=1,
       physics_world_step_size=self.TICKS_PER_FRAME/100,
