46f6c517d0a7a131af79b3237978ad17fed75ccc
Ethan Mathias <ethanmathias@gmail.com>
2026-02-12 11:05:13 -0500

    edits to lead car speed

--- tools/sim/bridge/metadrive/metadrive_bridge.py
+++ tools/sim/bridge/metadrive/metadrive_bridge.py
@@ -100,7 +100,7 @@ class MetaDriveBridge(SimulatorBridge):
       lead_speed_profile="ramp",
       lead_speed_start_mph=10.0,
       lead_speed_end_mph=30.0,
-      lead_speed_ramp_sec=20.0,
+      lead_speed_ramp_sec=90,
       lead_vehicle_lateral_offset=0.0,
       lead_vehicle_model="s",
       lead_vehicle_render=True,
