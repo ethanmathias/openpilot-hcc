695a7ebc80260aa2b5a431cf3ce151482f5d477d
Ethan Mathias <ethanmathias@gmail.com>
2026-02-26 13:24:17 -0500

    removed left right automatic steering

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -96,8 +96,10 @@ class Controls:
 
     # Check which actuators can be enabled
     standstill = abs(CS.vEgo) <= max(self.CP.minSteerSpeed, 0.3) or CS.standstill
-    CC.latActive = self.sm['selfdriveState'].active and not CS.steerFaultTemporary and not CS.steerFaultPermanent and \
-                   (not standstill or self.CP.steerAtStandstill)
+    # CC.latActive = self.sm['selfdriveState'].active and not CS.steerFaultTemporary and not CS.steerFaultPermanent and \
+    #                (not standstill or self.CP.steerAtStandstill)
+    # Manual-steering mode: keep longitudinal automation, disable all lateral actuation.
+    CC.latActive = False
     CC.longActive = CC.enabled and not any(e.overrideLongitudinal for e in self.sm['onroadEvents']) and (self.CP.openpilotLongitudinalControl or SIMULATION)
 
     actuators = CC.actuators
