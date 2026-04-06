1cc9d772ead4563844ec16839b3bf8ecff5f51c5
Ethan Mathias <ethanmathias@gmail.com>
2026-04-06 16:13:55 -0400

    Add ego V2V sim launcher

--- tools/sim/launch_openpilot_ego.sh
+++ tools/sim/launch_openpilot_ego.sh
@@ -15,9 +15,10 @@ export HCC_V2V_DEVICE_ID="${HCC_V2V_DEVICE_ID:-ego-sim}"
 export HCC_V2V_RELAY_HOST="${HCC_V2V_RELAY_HOST:-127.0.0.1}"
 export HCC_V2V_RELAY_PORT="${HCC_V2V_RELAY_PORT:-19090}"
 
-export BLOCK="${BLOCK},camerad,loggerd,encoderd,micd,logmessaged"
+<<<<<<< HEAD
+export BLOCK="${BLOCK},camerad,loggerd,encoderd,micd,logmessaged,ui"
 if [[ "$CI" ]]; then
-  export BLOCK="${BLOCK},ui"
+  export BLOCK="${BLOCK},modeld,dmonitoringmodeld,soundd"
 fi
 
 python3 -c "from openpilot.selfdrive.test.helpers import set_params_enabled; set_params_enabled()"
